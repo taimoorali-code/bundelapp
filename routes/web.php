3fe416bbe2832cd8b01de12fd2eedaca86a9ca25
taimoorali-code <9078472@gmail.com>
2025-09-03 11:26:04 +0500

    adding route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request; // instead of Facade
 
 /*
 |--------------------------------------------------------------------------
