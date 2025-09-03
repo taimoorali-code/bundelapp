f758065b845a88724ba6c3844dace9e5cf7d7143
taimoorali-code <9078472@gmail.com>
2025-09-03 16:10:02 +0500

    adding theme extention

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Bundle;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/get-bundle', function (Request $request) {
+    $shop = $request->query('shop');
+    $productId = $request->query('product_id');
+
+    $bundles = Bundle::where('shopify_product_id', $productId)
+        ->with('discounts')
+        ->get();
+
+    return response()->json($bundles);
+});
+
